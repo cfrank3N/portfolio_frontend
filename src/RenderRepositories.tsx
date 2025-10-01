12d930e9901bc56fe4d813997c01ccd8417c9939
cfrank3N <adamcecarfrank@gmail.com>
2025-10-01 12:25:54 +0200

    Added simple error handling

--- src/RenderRepositories.tsx
+++ src/RenderRepositories.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Alert } from "react-bootstrap";
 
 interface Repository {
     name: string,
@@ -8,17 +9,26 @@ interface Repository {
 
 
 export default function RenderRepositories() {
+    const [error, setError] = useState();
     const [repos, setRepos] = useState<Repository[]>([]);
     
     useEffect(() => {
         const fetchRepos = async () => {
-            const response = await fetch("https://portfolioapiadamfrank.azurewebsites.net/api/repos/pinned");
-            const repos: Repository[] = await response.json();
-            setRepos(repos);
+            try {
+                const response = await fetch("https://portfolioapiadamfrank.azurewebsites.net/api/repos/pinned");
+                const repos: Repository[] = await response.json();
+                setRepos(repos);
+            } catch (e: any) {
+                setError(e);
+            }
         };
        fetchRepos();
     }, 
     []);
+
+    if (error) {
+        return <Alert variant="warning">Couldn't fetch data</Alert>
+    }
 
     const listItems = repos.map(repo =>
         <li key={repo.name}>
